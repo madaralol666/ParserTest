a9499cc2cf88f276acc0bbcbbc63e6c13703fb1e
madaralol666 <ds409060@gmail.com>
2022-09-27 10:51:49 +0300

    Simple parser

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
-# link = "https://icanhazip.com/"
-# response = requests.get(link).text
-#     print("Success")
-# print(response)
-link = "https://vk.com/feed"
-Status_code = requests.get(link)
-response = requests.get(link).text
-if Status_code.status_code == 200:
-    print("success")
-print(response)
+header = {'user-agent': 'Madara'}
 
+link = "https://browser-info.ru/"
+response = requests.get(link, headers = header).text
+soup = BeautifulSoup(response, 'lxml')
+block = soup.find('div', id="tool_padding")
 
+# Check JavaScript
+check_js = block.find('div', id='javascript_check')
+status_js = check_js.find_all('span')[1].text
+result_js = f"JavaScript: {status_js}"
+
+# Check Cookie
+check_cookie = block.find('div', id = 'cookie_check')
+status_cookie = check_cookie.find_all('span')[1].text
+result_cookie = f"Cookie: {status_cookie}"
+
+# Check Flash
+check_flash = block.find('div', id = 'flash_version')
+status_flash = check_flash.find_all('span')[1].text
+result_flash = f"Flash: {status_flash}"
+
+# Check User-agent
+check_user = block.find('div', id = 'user_agent').text
+print(result_js)
+print(result_cookie)
+print(result_flash)
+print(check_user)
